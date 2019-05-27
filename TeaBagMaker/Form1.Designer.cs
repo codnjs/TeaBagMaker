0a4e9c3f577c9d196726d5ccfc2329fef6f584c0
zzex3on___yaho <s2017s34@gmail.com>
2019-05-27 09:57:34 +0900

    #1 콤보박스와 타이머 디자인 구현, 추가적으로 기능 일부 구현

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cbList = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.countBtn = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.lbresult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cbList
             // 
+            this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbList.FormattingEnabled = true;
             this.cbList.Location = new System.Drawing.Point(13, 13);
             this.cbList.Name = "cbList";
@@ -41,33 +45,52 @@
             this.cbList.TabIndex = 0;
             this.cbList.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
-            // button1
+            // countBtn
             // 
-            this.button1.Location = new System.Drawing.Point(13, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(207, 133);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "담그기!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.countBtn.Location = new System.Drawing.Point(13, 40);
+            this.countBtn.Name = "countBtn";
+            this.countBtn.Size = new System.Drawing.Size(207, 133);
+            this.countBtn.TabIndex = 1;
+            this.countBtn.Text = "담그기!";
+            this.countBtn.UseVisualStyleBackColor = true;
+            this.countBtn.Click += new System.EventHandler(this.CountBtn_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // lbresult
+            // 
+            this.lbresult.AutoSize = true;
+            this.lbresult.Location = new System.Drawing.Point(67, 204);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(105, 12);
+            this.lbresult.TabIndex = 2;
+            this.lbresult.Text = "시간이 표시됩니다";
+            this.lbresult.Click += new System.EventHandler(this.Label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 288);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(234, 253);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.countBtn);
             this.Controls.Add(this.cbList);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox cbList;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button countBtn;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label lbresult;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -30,13 +30,83 @@ namespace TeaBagMaker
             {
                 this.cbList.Items.Add(this.List[i]);
             }
-            this.OrgStr = this.
+            this.cbList.SelectedIndex = 0;
 
         }
 
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if(this.cbList.SelectedIndex == 0) // 홍차 ,, 이미 0이 선택되어있음 ㅠ
+            {
+                
+            }
+            else if(this.cbList.SelectedIndex == 1) // 녹차
+            {
 
+            }
+            else if (this.cbList.SelectedIndex == 2) // 루이보스차
+            {
+
+            }
+            else if (this.cbList.SelectedIndex == 3) // 국화차
+            {
+
+            }
+        }
+
+        private bool Timer_Tick()
+        {
+            if (this.cbList.SelectedIndex == 0) // 홍차 ,, 이미 0이 선택되어있음 ㅠ
+            {
+                this.Timer.Interval = 2000;
+                this.Timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "티백이 모두 우려졌습니다",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else if (this.cbList.SelectedIndex == 1) // 녹차
+            {
+                this.Timer.Interval = 3000;
+                this.Timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "티백이 모두 우려졌습니다",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else if (this.cbList.SelectedIndex == 2) // 루이보스차
+            {
+                this.Timer.Interval = 5000;
+                this.Timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "티백이 모두 우려졌습니다",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else if (this.cbList.SelectedIndex == 3) // 국화차
+            {
+                this.Timer.Interval = 2000;
+                this.Timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "티백이 모두 우려졌습니다",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("어딘가 잘못되었습니다", "경고",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CountBtn_Click(object sender, EventArgs e)
+        {
+            if(Timer_Tick())
+            {
+
+            }
         }
     }
 }
